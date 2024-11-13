143a111f474500df3210739fabfd1b6772f67577
henokyohanesdev <henokyohanes8@gmail.com>
2024-11-12 22:00:04 -0600

    productDetail component with fully responsive styling implemented.

--- src/pages/productDetail/productDetail.jsx
+++ src/pages/productDetail/productDetail.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Layout from "../../Components/Layout/Layout";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import SingleProduct from "../../Components/Products/SingleProduct/SingleProduct";
+import Loader from "../../Components/Loader/Loader";
+import styles from "./ProductDetail.module.css";
 
-function productDetail() {
+const ProductDetail = () => {
+  const { productId } = useParams();
+  const [product, setProduct] = useState({});
+  const [Loadeing, setLoadering] = useState(false);
+
+  useEffect(() => {
+    setLoadering(true);
+    axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .then((res) => {
+        setProduct(res.data);
+        setLoadering(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [productId]);
   return (
-    <div>
-      <h1>productDetail</h1>
-    </div>
-  )
+    <Layout>
+      {Loadeing && <Loader />}
+      <p className={styles.title}>Product Detail</p>
+      <div className={styles.product_detail}>
+        <SingleProduct
+          {...product}
+          flex={true}
+          detail={true}
+          addButton={true}
+        />
+      </div>
+    </Layout>
+  );
 }
 
-export default productDetail
+export default ProductDetail

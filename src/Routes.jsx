a8fbe8ab2791dc89663c40b0fed9aaa406e08457
henokyohanesdev <henokyohanes8@gmail.com>
2024-11-14 03:17:51 -0600

    payment page ui with functionality implemented.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -6,23 +6,27 @@ import Account from './pages/Auth/Account'
 import Payment from './pages/Payment/Payment'
 import Orders from './pages/Orders/Orders'
 import Cart from './pages/Cart/Cart'
-import Results from './pages/Results/Results' 
+import Results from './pages/Results/Results'
 import ProductDetail from './pages/ProductDetail/ProductDetail'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
 
 export default function Routing() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/auth" element={<Auth />} />
-        <Route exact path="/account" element={<Account />} />
-        <Route exact path="/payment" element={<Payment />} />
-        <Route exact path="/orders" element={<Orders />} />
-        <Route exact path="/cart" element={<Cart />} />
-        <Route exact path="/category/:categoryName" element={<Results />} />
-        <Route exact path="/products/:productId" element={<ProductDetail />} />
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/auth' element={<Auth />} />
+        <Route exact path='/account' element={<Account />} />
+        <Route exact path='/payment' element={<Elements stripe={stripePromise}> <Payment /> </Elements>} />
+        <Route exact path='/orders' element={<Orders />} /> 
+        <Route exact path='/cart' element={<Cart />} />
+        <Route exact path='/category/:categoryName' element={<Results />} />
+        <Route exact path='/products/:productId' element={<ProductDetail />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
--- src/pages/payment/payment.jsx
+++ src/pages/payment/payment.jsx
@@ -1,11 +1,120 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import Layout from "../../Components/Layout/Layout";
+import { DataContext } from "../../Components/DataProvider/DataProvider";
+import { FaLock } from "react-icons/fa6";
+import { useElements, useStripe, CardElement } from "@stripe/react-stripe-js";
+import { axiosInstance } from "../../utils/axios";
+import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormat";
+import { FadeLoader } from "react-spinners";
+import { db } from "../../utils/firebase";
+import { doc, setDoc, collection } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { ActionTypes } from "../../utils/actionType";
+import SingleProduct from "../../Components/Products/SingleProduct/SingleProduct";
 
-const payment = () => {
+export default function Payment() {
+  const [{ user, cart }, dispatch] = useContext(DataContext);
+  const total = cart.reduce((total, item) => total + item.quantity, 0);
+  const totalPrice = cart.reduce(
+    (total, item) => total + item.quantity * item.price,
+    0
+  );
+  const stripe = useStripe();
+  const elements = useElements();
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = async (event) => {
+    event.error ? setError(event.error.message) : setError(null);
+  };
+
+  const handlePayment = async (e) => {
+    e.preventDefault();
+
+    try {
+      setProcessing(true);
+      const res = await axiosInstance.post(
+        `/payment/create?totalPrice=${totalPrice * 100}`
+      );
+
+      const clientSecret = res.data.clientSecret;
+
+      const confirmation = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      });
+
+      const { paymentIntent } = confirmation;
+      console.log("PaymentIntent:", paymentIntent);
+      console.log("user:", user.uid);
+
+      console.log("start");
+
+      await setDoc(
+        doc(collection(db, "users"), user.uid, "orders", paymentIntent.id),
+        {
+          cart: cart,
+          amount: paymentIntent.amount,
+          created: paymentIntent.created,
+        }
+      );
+
+      dispatch({ type: ActionTypes.RESET_CART });
+      console.log("end");
+      setProcessing(false);
+      navigate("/orders", { state: { msg: "Payment successful!" } });
+    } catch (error) {
+      setProcessing(false);
+      setError(error.message);
+      console.log(error);
+    }
+  };
   return (
-    <div>
-      <h1>payment</h1>
-    </div>
-  )
+    <Layout>
+      <div>
+        <img src="" alt="amazon logo" />
+        <p>
+          Checkout (<span>{cart && total} items</span>)
+        </p>
+        <FaLock />
+      </div>
+      <div>
+        <p>Delivery address</p>
+        <div>
+          <p>{user?.email}</p>
+          <p>123456</p>
+          <p>12121</p>
+          <p>tx</p>
+        </div>
+      </div>
+      <div>
+        <p>Payment method</p>
+        <div>
+          <form onSubmit={handlePayment}>
+            {error && <small>{error}</small>}
+            <CardElement onChange={handleChange} />
+          </form>
+        </div>
+      </div>
+      <div>
+        <p>Order summary</p>
+        <div>
+          {cart &&
+            cart.map((item) => (
+              <SingleProduct key={item.id} {...item} flex />
+            ))}
+        </div>
+            <div>
+              <span>
+                total order: <CurrencyFormat value={totalPrice} />
+              </span>
+            </div>
+            <button type="submit" disabled={processing}>
+              {processing ? <FadeLoader /> : "Pay"}
+            </button>
+      </div>
+    </Layout>
+  );
 }
-
-export default payment

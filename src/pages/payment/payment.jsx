2ae82e3d5ea2299c3f80e470fb088ba6780e3a0b
henokyohanesdev <henokyohanes8@gmail.com>
2024-11-14 17:38:45 -0600

    payment page fully responsive styling implemented.

--- src/pages/payment/payment.jsx
+++ src/pages/payment/payment.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useRef } from "react";
 import Layout from "../../Components/Layout/Layout";
 import { DataContext } from "../../Components/DataProvider/DataProvider";
 import { FaLock } from "react-icons/fa6";
@@ -11,6 +11,8 @@ import { doc, setDoc, collection } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { ActionTypes } from "../../utils/actionType";
 import SingleProduct from "../../Components/Products/SingleProduct/SingleProduct";
+import logo from "../../assets/Images/amazon-logo-1.png";
+import styles from "./Payment.module.css";
 
 export default function Payment() {
   const [{ user, cart }, dispatch] = useContext(DataContext);
@@ -24,6 +26,15 @@ export default function Payment() {
   const [error, setError] = useState(null);
   const [processing, setProcessing] = useState(false);
   const navigate = useNavigate();
+  const formRef = useRef(null); // Create a form reference
+
+  const cardElementOptions = {
+    style: {
+      base: {
+        fontSize: "clamp(.8em, 4vw, 1.1em)"
+      },
+    },
+  };
 
   const handleChange = async (event) => {
     event.error ? setError(event.error.message) : setError(null);
@@ -73,47 +84,53 @@ export default function Payment() {
   };
   return (
     <Layout>
-      <div>
-        <img src="" alt="amazon logo" />
-        <p>
-          Checkout (<span>{cart && total} items</span>)
+      <div className={styles.checkout_header}>
+        <img src={logo} alt="amazon logo" className={styles.logo} />
+        <p className={styles.checkout_title}>
+          Checkout (
+          <span className={styles.checkout_count}>{cart && total} items</span>)
         </p>
         <FaLock />
       </div>
-      <div>
-        <p>Delivery address</p>
-        <div>
+      <div className={styles.checkout_address}>
+        <p className={styles.all_titles}>1 - Shipping address</p>
+        <div className={styles.address}>
           <p>{user?.email}</p>
-          <p>123456</p>
-          <p>12121</p>
-          <p>tx</p>
+          <p>12345 street dr, Houston TX, 77777</p>
+          <p>United States</p>
         </div>
+        <p className={styles.change}>Change</p>
       </div>
-      <div>
-        <p>Payment method</p>
-        <div>
+      <div className={styles.checkout_payment}>
+        <p className={styles.all_titles}>2 - Payment method</p>
+        <div className={styles.payment}>
           <form onSubmit={handlePayment}>
-            {error && <small>{error}</small>}
-            <CardElement onChange={handleChange} />
+            <CardElement onChange={handleChange} options={cardElementOptions} />
+            {error && <small className={styles.error}>{error}</small>}
           </form>
         </div>
+        <p className={styles.change}>Change</p>
       </div>
-      <div>
-        <p>Order summary</p>
-        <div>
+      <div className={styles.checkout_summary}>
+        <p className={styles.all_titles}>3 - Order summary</p>
+        <div className={styles.products}>
           {cart &&
-            cart.map((item) => (
-              <SingleProduct key={item.id} {...item} flex />
-            ))}
+            cart.map((item) => <SingleProduct key={item.id} {...item} flex />)}
         </div>
-            <div>
-              <span>
-                total order: <CurrencyFormat value={totalPrice} />
-              </span>
-            </div>
-            <button type="submit" disabled={processing}>
-              {processing ? <FadeLoader /> : "Pay"}
-            </button>
+      </div>
+      <div className={styles.place_order}>
+        <button
+          className={styles.place_order_btn}
+          type="submit"
+          disabled={processing}
+          onClick={() => formRef.current.requestSubmit()}
+        >
+          {processing ? <FadeLoader /> : "place your order"}
+        </button>
+        <span>
+          <p className={styles.order_total}>order total: <CurrencyFormat value={totalPrice} /></p>
+          <p className={styles.notice}>By placing your order, you agree to my Amazon-clone notice and conditions of use.</p>
+        </span>
       </div>
     </Layout>
   );

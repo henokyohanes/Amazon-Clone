8aa669629bc9ae891d1444591e32ac770e90734b
henokyohanesdev <henokyohanes8@gmail.com>
2024-11-14 23:36:53 -0600

    sign out page with fully responsive styling implemented.

--- src/pages/auth/account.jsx
+++ src/pages/auth/account.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
+import Layout from '../../Components/Layout/Layout'
 import styles from './Auth.module.css'
 import { auth } from '../../utils/firebase'
 import { Link } from 'react-router-dom'
 
 export default function Account() {
   return (
-    <div>
-      <Link to='/auth' className={styles.button} onClick={() => auth.signOut()}>sign out</Link>
-    </div>
+    <Layout>
+      <div className={styles.signout_container}>
+        <p className={styles.signout_title}>Your Account</p>
+        <p className={styles.signout_text}>Manage your account</p>
+        <p className={styles.signout_text}>Customer Service</p>
+        <p className={styles.signout_text}>Return Policy</p>
+        <p className={styles.signout_text}>Settings</p>
+      </div>
+      <Link to='/auth' className={styles.signout_button} onClick={() => auth.signOut()}>sign out</Link>
+    </Layout>
   )
 }

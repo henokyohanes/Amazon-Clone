bf1c7086f83fb0a25d10f79119100e052f3aee73
henokyohanesdev <henokyohanes8@gmail.com>
2024-11-14 18:54:17 -0600

    route protection implemented for payment and orders page.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -10,6 +10,7 @@ import Results from './pages/Results/Results'
 import ProductDetail from './pages/ProductDetail/ProductDetail'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
+import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute"; 
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
 
@@ -20,8 +21,8 @@ export default function Routing() {
         <Route exact path='/' element={<Home />} />
         <Route exact path='/auth' element={<Auth />} />
         <Route exact path='/account' element={<Account />} />
-        <Route exact path='/payment' element={<Elements stripe={stripePromise}> <Payment /> </Elements>} />
-        <Route exact path='/orders' element={<Orders />} /> 
+        <Route exact path='/payment' element={<ProtectedRoute msg="Please login first" redirect="/payment"> <Elements stripe={stripePromise}> <Payment /> </Elements> </ProtectedRoute>} />
+        <Route exact path='/orders' element={<ProtectedRoute msg="Please login first" redirect="/orders"> <Orders /> </ProtectedRoute>} />
         <Route exact path='/cart' element={<Cart />} />
         <Route exact path='/category/:categoryName' element={<Results />} />
         <Route exact path='/products/:productId' element={<ProductDetail />} />
